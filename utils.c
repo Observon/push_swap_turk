d83d7616339b937f10cca22f145e0aeb50f4fc2d
Observon <erickoliveirarangel@gmail.com>
2026-01-20 20:15:15 -0300

    add utils.c with utility functions for stack management

---
+++ utils.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eride-ol <eride-ol@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 17:19:06 by eride-ol          #+#    #+#             */
+/*   Updated: 2026/01/20 17:19:06 by eride-ol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void error_exit(void)
+{
+    write(2, "Error\n", 6);
+    exit(1);
+}
+
+void free_stack(t_stack **stack)
+{
+    t_stack *tmp;
+    
+    if (!stack || !*stack)
+        return;
+    while (*stack)
+    {
+        tmp = (*stack)->next;
+        free(*stack);
+        *stack = tmp;
+    }
+    *stack = NULL;
+}
+
+int stack_size(t_stack *stack)
+{
+    int size;
+    
+    size = 0;
+    while (stack)
+    {
+        size++;
+        stack = stack->next;
+    }
+    return (size);
+}
+
+t_stack *get_bottom(t_stack *stack)
+{
+    while (stack && stack->next)
+        stack = stack->next;
+    return (stack);
+}
+
+void stack_add_back(t_stack **stack, t_stack *new)
+{
+    t_stack *tail;
+    
+    if (!new)
+        return;
+    if (!*stack)
+    {
+        *stack = new;
+        return;
+    }
+    tail = get_bottom(*stack);
+    tail->next = new;
+}
+
+t_stack *create_node(int value)
+{
+    t_stack *new;
+    
+    new = malloc(sizeof(t_stack));
+    if (!new)
+        return (NULL);
+    new->value = value;
+    new->index = 0;
+    new->pos = 0;
+    new->target_pos = 0;
+    new->cost_a = 0;
+    new->cost_b = 0;
+    new->next = NULL;
+    return (new);
+}
