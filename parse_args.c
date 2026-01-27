e24a0991c9af4571aab58c8367d77369c9d47b87
Observon <erickoliveirarangel@gmail.com>
2026-01-27 01:24:07 -0300

    refactor: streamline argument processing in parse_args.c

--- parse_args.c
+++ parse_args.c
@@ -14,72 +14,85 @@
 
 static int	validate_and_add(char *num_str, t_stack **stack_a)
 {
-    int num;
+	int	num;
 
-    if (!is_valid_number(num_str))
-        return 0;
-    if (!parse_int(num_str, &num))
-        return 0;
-    stack_add_back(stack_a, create_node(num));
-    return 1;
+	if (!is_valid_number(num_str))
+		return (0);
+	if (!parse_int(num_str, &num))
+		return (0);
+	stack_add_back(stack_a, create_node(num));
+	return (1);
 }
 
-static void free_split(char **split)
+static void	free_split(char **split)
 {
-    int i = 0;
-    
-    if (!split)
-        return;
-    while (split[i])
-    {
-        free(split[i]);
-        i++;
-    }
-    free(split);
+	int	i;
+
+	i = 0;
+	if (!split)
+		return ;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
 }
 
-static int process_split(char **nums, t_stack **stack_a)
+static int	process_split(char **nums, t_stack **stack_a)
 {
-    int j = 0;
-    
-    while (nums[j])
-    {
-        if (!validate_and_add(nums[j], stack_a))
-        {
-            free_split(nums);
-            free_stack(stack_a);
-            error_exit();
-        }
-        j++;
-    }
-    return 1;
+	int	j;
+
+	j = 0;
+	while (nums[j])
+	{
+		if (!validate_and_add(nums[j], stack_a))
+		{
+			free_split(nums);
+			free_stack(stack_a);
+			error_exit();
+		}
+		j++;
+	}
+	return (1);
 }
 
-t_stack *parse_arguments(int argc, char **argv)
+static void	process_arg(char *arg, t_stack **stack_a)
 {
-    t_stack *stack_a = NULL;
-    char **nums;
-    int i = 1;
-    
-    while (i < argc)
-    {
-        if (!argv[i] || !*argv[i])
-            error_exit();
-        nums = ft_split(argv[i], ' ');
-        if (!nums || !nums[0])
-        {
-            free_split(nums);
-            free_stack(&stack_a);
-            error_exit();
-        }
-        process_split(nums, &stack_a);
-        free_split(nums);
-        i++;
-    }
-    if (has_duplicates(stack_a))
-    {
-        free_stack(&stack_a);
-        error_exit();
-    }
-    return stack_a;
+	char	**nums;
+
+	if (!arg || !*arg)
+	{
+		free_stack(stack_a);
+		error_exit();
+	}
+	nums = ft_split(arg, ' ');
+	if (!nums || !nums[0])
+	{
+		free_split(nums);
+		free_stack(stack_a);
+		error_exit();
+	}
+	process_split(nums, stack_a);
+	free_split(nums);
+}
+
+t_stack	*parse_arguments(int argc, char **argv)
+{
+	t_stack	*stack_a;
+	int		i;
+
+	stack_a = NULL;
+	i = 1;
+	while (i < argc)
+	{
+		process_arg(argv[i], &stack_a);
+		i++;
+	}
+	if (has_duplicates(stack_a))
+	{
+		free_stack(&stack_a);
+		error_exit();
+	}
+	return (stack_a);
 }
